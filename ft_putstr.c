fefd41cfb6f4da3f65005d32bc76563c895ff486
maherraz <maherraz@student.42.mg>
2026-03-02 09:00:00 +0300

    feat: add ft_putstr

---
+++ ft_putstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maherraz <maherraz@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 10:42:40 by maherraz          #+#    #+#             */
+/*   Updated: 2026/03/02 10:45:46 by maherraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putstr(char *str)
+{
+	int	count;
+
+	count = 0;
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		count += ft_putchar(*str);
+		str++;
+	}
+	return (count);
+}
